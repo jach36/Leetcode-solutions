cf4cef729d7a418ea046fdb4f0c543533ba1865c
Jiwoo Chung <aaronjiwoo7963@gmail.com>
2025-09-15 13:29:13 +0900

    Time: 0 ms (100%), Space: 13.3 MB (64.62%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            middle = (left + right)//2
+            if nums[middle] == target:
+                return middle
+            if nums[middle] > target:
+                right = middle - 1
+            else:
+                left = middle + 1
+        return -1
