ff31f2eef88b3a97a2a098b7366d02c60f187e49
Jiwoo Chung <aaronjiwoo7963@gmail.com>
2025-09-15 13:34:48 +0900

    Time: 136 ms (21.85%), Space: 13.1 MB (61.15%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        new_s = ""
+        for c in s:
+            if c.isalnum():
+                new_s += c.lower()
+        
+        return new_s == new_s[::-1]
+
+        
