e1f736e39206c95712a5fa71ab277d1e3aace9af
Jiwoo Chung <aaronjiwoo7963@gmail.com>
2025-09-15 13:46:52 +0900

    Time: 235 ms (17.08%), Space: 16 MB (45.87%) - LeetHub

---
+++ 0706-design-hashmap/0706-design-hashmap.py
@@ -0,0 +1,68 @@
+class ListNode:
+    def __init__(self, key=-1 , val=-1, next = None):
+        self.key = key
+        self.value = val
+        self.next = next
+
+
+class MyHashMap(object):
+
+    def __init__(self):
+        self.map = [ListNode() for i in range(1000)]
+        
+    def hash(self, key):
+        return key % len(self.map)
+
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: None
+        """
+        cur = self.map[self.hash(key)]
+        while (cur.next):
+            if cur.next.key == key:
+                cur.next.value = value
+                return
+            cur = cur.next
+        cur.next = ListNode(key,value)
+        
+
+        
+
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+
+        cur = self.map[self.hash(key)].next
+        while (cur):
+            if (cur.key == key):
+                return cur.value
+            cur = cur.next 
+        return -1
+
+        
+
+    def remove(self, key):
+        """
+        :type key: int
+        :rtype: None
+        """
+        cur = self.map[self.hash(key)]
+        while (cur and cur.next):
+            if (cur.next.key == key):
+                cur.next = cur.next.next
+                return
+            cur =cur.next
+
+
+        
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
