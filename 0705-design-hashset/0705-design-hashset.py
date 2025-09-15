1eda7c8ba05a14b4da0a347470571243f5e9e83f
Jiwoo Chung <aaronjiwoo7963@gmail.com>
2025-09-15 13:46:04 +0900

    Time: 255 ms (20.83%), Space: 24.9 MB (25.1%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.py
@@ -0,0 +1,62 @@
+class ListNode:
+    def __init__(self, key):
+        self.key = key
+        self.next = None
+
+class MyHashSet(object):
+
+
+    def __init__(self):
+        self.set = [ListNode(0) for i in range(10**4)]
+
+    def hash(self, key):
+        return key % len(self.set)
+
+    def add(self, key):
+        """
+        :type key: int
+        :rtype: None
+        """
+        cur = self.set[key % len(self.set)]
+        
+        while cur.next:
+            if cur.next.key == key:
+                return 
+            cur = cur.next
+        cur.next = ListNode(key)
+
+    def remove(self, key):
+        """
+        :type key: int
+        :rtype: None
+        """
+        cur = self.set[key % len(self.set)]
+
+        while cur.next:
+            if cur.next.key == key:
+                cur.next = cur.next.next 
+                return 
+            cur = cur.next
+        
+
+    def contains(self, key):
+        """
+        :type key: int
+        :rtype: bool
+        """
+        
+        cur = self.set[key % len(self.set)]
+        
+        while cur.next:
+            if cur.next.key == key:
+                return True
+            cur = cur.next
+        return False
+        
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
